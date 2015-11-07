7b5e1e25b7e35842ee11d89bab587aee97ddda7f
jamespjbennett <jamespjbennett@hotmail.com>
2015-11-07 17:12:46 +0000

    added margin to the about us page so it doesnt jump when nav bar becomes fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,9 +43,12 @@ $(document).ready(function(){
         console.log($(window).scrollTop())
       if ($(window).scrollTop() > 801) {
         $('.navbar').addClass('navbar-fixed-top');
+        $('.homepage-about-us').css('margin-top', '80px');
       }
       if ($(window).scrollTop() < 800) {
         $('.navbar').removeClass('navbar-fixed-top');
+        $('.homepage-about-us').css('margin-top', '0px');
+
       }
     });
 });
