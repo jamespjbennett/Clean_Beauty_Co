b1ae1dca60a13cda9ff841f82f376b190a35712e
jamespjbennett <jamespjbennett@hotmail.com>
2015-11-19 21:29:51 +0000

    migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119212541) do
+ActiveRecord::Schema.define(version: 20151119212830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blog_entries", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.date     "date"
     t.text     "content"
     t.string   "title"
     t.string   "image"
+    t.text     "Introduction_text"
   end
 
   create_table "ingredients", force: :cascade do |t|
