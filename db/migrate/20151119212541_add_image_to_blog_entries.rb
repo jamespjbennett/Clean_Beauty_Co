ec0e16811ca7f5e46b8335d5c72805006b564078
jamespjbennett <jamespjbennett@hotmail.com>
2015-11-19 21:26:48 +0000

    added image attribute to the blog entries

---
+++ db/migrate/20151119212541_add_image_to_blog_entries.rb
@@ -0,0 +1,5 @@
+class AddImageToBlogEntries < ActiveRecord::Migration
+  def change
+    add_column :blog_entries, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102222254) do
+ActiveRecord::Schema.define(version: 20151119212541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151102222254) do
     t.date     "date"
     t.text     "content"
     t.string   "title"
+    t.string   "image"
   end
 
   create_table "ingredients", force: :cascade do |t|
