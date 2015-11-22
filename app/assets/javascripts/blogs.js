b1527341f7faffe55bf2f8795c022e5a96ab640b
jamespjbennett <jamespjbennett@hotmail.com>
2015-11-22 18:25:49 +0000

    got a javascript function working that displays the next set of non visible elements

--- app/assets/javascripts/blogs.js
+++ app/assets/javascripts/blogs.js
@@ -2,8 +2,14 @@ function revealMoreBlogPosts(){
   var allBlogEntries = $('.featured-blog-entry-container');
   var allVisibleBlogEntries = $('.featured-blog-entry-container:visible');
   var nonVisibleBlogEntries = $('.featured-blog-entry-container:hidden');
-  var nonVisibleBlogEntriesToShow = allVisibleBlogEntries.slice(0, 6);
-  nonVisibleBlogEntriesToShow.show();
+  var firstNonVisible = allVisibleBlogEntries.last().next();
+  var nonVisibleBlogEntriesToShow = nonVisibleBlogEntries.slice(0, 6);
+  for(i=0;i<6;i++){
+    firstNonVisible.show();
+    var firstNonVisible = $('.featured-blog-entry-container:visible').last().next();
+  }
+  
+
   allVisibleBlogEntries.hide();
 }
 
