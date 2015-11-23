3fbaaf67bc5e2b13c621b4394708c6b2b6b2a191
jamespjbennett <jamespjbennett@hotmail.com>
2015-11-23 20:51:41 +0000

    further changes to the attempted enabling of fonts on herkou

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module CleanBeautyCoApp
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
   end
 end
