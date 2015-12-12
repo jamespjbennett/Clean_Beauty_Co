ddee5700616fa6e65835bd105d4942f78ff1a23c
jamespjbennett <jamespjbennett@hotmail.com>
2015-12-12 11:37:27 +0000

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129194625) do
+ActiveRecord::Schema.define(version: 20151210204821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,15 @@ ActiveRecord::Schema.define(version: 20151129194625) do
     t.text     "uri_cache"
     t.string   "simplified_type",        default: "file"
   end
+
+  create_table "simple_captcha_data", force: :cascade do |t|
+    t.string   "key",        limit: 40
+    t.string   "value",      limit: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "simple_captcha_data", ["key"], name: "idx_key", using: :btree
 
   create_table "stockists", force: :cascade do |t|
     t.datetime "created_at", null: false
