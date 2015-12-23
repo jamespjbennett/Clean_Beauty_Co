0260666442694845c7e9d67d682d05722d58b16f
James Bennett <jamesbennett@elsies-air.home>
2015-12-23 18:34:25 +0000

    added in smaller images. Adjusted the max width of about page images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include SimpleCaptcha::ControllerHelpers
   protect_from_forgery with: :exception
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+       false
+    else
+      "application"
+    end
+  end
+
+
 end
