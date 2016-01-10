b6935b2ceddc0e68d18c1838a5f6b9c9d58cea08
James Bennett <jamespjbennett@hotmail.com>
2016-01-10 10:10:12 +0000

    changed the display for back to all recipes button

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { :host => 'clean-beauty-co.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'clean-beauty-co.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'http://clean-beauty-co.herokuapp.com/' }
+  Rails.application.routes.default_url_options[:host] = 'http://clean-beauty-co.herokuapp.com/'
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
