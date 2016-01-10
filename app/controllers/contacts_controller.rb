97054970193b2a35ffada2e8a6e4aa7187709c17
James Bennett <jamespjbennett@hotmail.com>
2016-01-10 17:54:03 +0000

    changed mail devliery method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,16 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(params[:contact])
-    @contact.request = request
+    subject = params[:contact][:subject]
+    message = params[:contact][:message]
+    if !subject
+      subject = "no subject"
+    end
+    if !message
+      subject = "no message"
+    end
+    ActionMailer::Base.mail(from: params[:contact][:email], to: "jamespjbennett@gmail.com", subject: params[:contact][:subject], body: params[:contact][:message]).deliver_now
+    # @contact.request = request
     if @contact.deliver
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
     else
