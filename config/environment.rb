365ff1810262d1f93a42f64f8530b241a3dd008e
James Bennett <jamespjbennett@hotmail.com>
2016-01-10 15:55:11 +0000

    changes to the email env varaibles

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,11 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
+
+# Load the app's custom environment variables here, so that they are loaded before environments/*.rb
+app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
+load(app_environment_variables) if File.exists?(app_environment_variables)
+
+
+
 Rails.application.initialize!
