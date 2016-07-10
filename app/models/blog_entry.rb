5304d870439df010d5d90f447b25abe996fb899a
James Bennett <jamespjbennett@hotmail.com>
2016-07-10 18:49:53 +0100

    further friendly id config

--- app/models/blog_entry.rb
+++ app/models/blog_entry.rb
@@ -1,8 +1,9 @@
 class BlogEntry < ActiveRecord::Base
+	# extend FriendlyId
+	# friendly_id :slug, use: :slugged
 	acts_as_commontator
 	acts_as_commontable
 	has_many :blog_ccomments
-	friendly_id :slug, use: :slugged
 	serialize :keywords, Array
 
 end
